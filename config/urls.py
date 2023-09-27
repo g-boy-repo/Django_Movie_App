b78e451cec32acb09fdc0f0e835942084ebf0c33
G-boy75 <glul71597@gmail.com>
2023-09-27 18:12:53 -0400

    Creating Navigation Using Template Inheritance

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from movies.views import about, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('about/', about)
+    path('', index, name="home"),
+    path('about/', about, name="about")
 ]
