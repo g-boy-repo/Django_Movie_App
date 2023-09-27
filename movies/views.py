1f92859659a96fa48215d686522e1c856fd4f56d
G-boy75 <glul71597@gmail.com>
2023-09-27 17:47:39 -0400

    Listing Out Our Favorite Movies

--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return render(request, 'movies/index.html', {'movie': 'The Matrix'})
+    context = {
+        'movies': ['gladiator', 'titanic', 'the matrix', 'the notebook', 'star wars']
+    }
+    return render(request, 'movies/index.html', context)
 
 
 def about(request):
