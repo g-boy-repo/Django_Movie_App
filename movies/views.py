930df3c358c93a9fe5e57b7393dcd4e59f95337d
G-boy75 <glul71597@gmail.com>
2023-09-27 17:41:09 -0400

    Accessing Variables in Django Templates

--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return render(request, 'movies/index.html')
+    return render(request, 'movies/index.html', {'movie': 'The Matrix'})
 
 
 def about(request):
